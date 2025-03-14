c797fb084f5328520d40ccc8933e76e5916f1ef6
Harry Huang <harryhuang2652@qq.com>
2025-03-14 22:45:59 +0800

    feat: announcement read mark

--- desktop/src/cn/harryh/arkpets/controllers/AnnounceDialog.java
+++ desktop/src/cn/harryh/arkpets/controllers/AnnounceDialog.java
@@ -3,6 +3,7 @@
  */
 package cn.harryh.arkpets.controllers;
 
+import cn.harryh.arkpets.ArkConfig;
 import cn.harryh.arkpets.ArkHomeFX;
 import cn.harryh.arkpets.guitasks.FetchAnnounceTask;
 import cn.harryh.arkpets.guitasks.GuiTask.GuiTaskStyle;
@@ -11,6 +12,7 @@ import cn.harryh.arkpets.utils.Logger;
 import cn.harryh.arkpets.utils.NetUtils;
 import cn.harryh.arkpets.utils.markdown.FxmlConvertor;
 import cn.harryh.arkpets.utils.markdown.FxmlDocumentController;
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.fastjson.annotation.JSONField;
 import com.jfoenix.controls.*;
 import javafx.application.Platform;
@@ -18,11 +20,18 @@ import javafx.collections.FXCollections;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Group;
 import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.SVGPath;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Consumer;
+import java.util.stream.Collectors;
 
 
 public final class AnnounceDialog implements Controller<ArkHomeFX> {
@@ -48,6 +57,8 @@ public final class AnnounceDialog implements Controller<ArkHomeFX> {
 
     private JFXListCell<AnnounceItem> selectedAnnoCell;
 
+    private AnnounceReadHandler announceReadHandler;
+
     private ArkHomeFX app;
 
     @Override
@@ -62,6 +73,8 @@ public final class AnnounceDialog implements Controller<ArkHomeFX> {
 
         annoRefetch.setOnAction(e -> this.fetchAnnounce());
 
+        announceReadHandler = new AnnounceReadHandler(app.config);
+
         Platform.runLater(() -> GuiPrefabs.disableScrollPaneCache(annoScroll));
     }
 
@@ -72,9 +85,10 @@ public final class AnnounceDialog implements Controller<ArkHomeFX> {
         annoListView.getItems().clear();
         annoItemList.addListener((ListChangeListener<AnnounceItem>) change -> {
             change.next();
-            if (change.wasAdded()) {
+            if (change.wasAdded() && change.getAddedSize() > 0) {
                 Logger.info("Announce", "Fetched " + change.getAddedSize() + " announcements");
                 change.getAddedSubList().forEach(anno -> annoListView.getItems().add(createCell(anno)));
+                announceReadHandler.retainAll(change.getAddedSubList());
             }
             if (!annoListView.getItems().isEmpty()) {
                 annoListView.setFixedCellSize(40);
@@ -85,22 +99,38 @@ public final class AnnounceDialog implements Controller<ArkHomeFX> {
     }
 
     private JFXListCell<AnnounceItem> createCell(AnnounceItem anno) {
-        double width = annoListView.getPrefWidth() * 0.799;
+        double width = annoListView.getPrefWidth() * 0.75;
+        double offset = width * 0.175;
         JFXListCell<AnnounceItem> cell = new JFXListCell<>();
         cell.getStyleClass().addAll("list-item");
+        cell.setPrefWidth(width);
+        cell.setItem(anno);
+        cell.setId(anno.getAnnoId());
+        SVGPath dot = GuiPrefabs.Icons.getIcon(GuiPrefabs.Icons.SVG_DIAMOND, GuiPrefabs.COLOR_WARNING);
+        dot.setLayoutX(-offset);
+        dot.setScaleX(0.6);
+        dot.setScaleY(0.6);
         Label name = new Label(anno.title);
         name.getStyleClass().addAll("list-item-label");
-        name.setPrefWidth(width);
-        cell.setPrefWidth(width);
-        cell.setGraphic(name);
-        cell.setItem(anno);
+        cell.setGraphic(new Group(dot, name));
+        refreshCellGraphic(cell);
         return cell;
+    }
+
+    private void refreshCellGraphic(JFXListCell<AnnounceItem> cell) {
+        double width = annoListView.getPrefWidth() * 0.75;
+        double offset = width * 0.175;
+        SVGPath dot = (SVGPath) ((Group) (cell.getGraphic())).getChildrenUnmodifiable().get(0);
+        dot.setVisible(!announceReadHandler.isRead(cell.getItem()));
+        Label name = (Label) ((Group) (cell.getGraphic())).getChildrenUnmodifiable().get(1);
+        name.setPrefWidth(width - (announceReadHandler.isRead(cell.getItem()) ? 0 : offset));
     }
 
     private void selectCell(JFXListCell<AnnounceItem> cell) {
         // Reset
         if (selectedAnnoCell != null) {
             selectedAnnoCell.getStyleClass().setAll("list-item");
+            refreshCellGraphic(selectedAnnoCell);
         }
         selectedAnnoCell = cell;
         selectedAnnoCell.getStyleClass().add("list-item-active");
@@ -135,6 +165,50 @@ public final class AnnounceDialog implements Controller<ArkHomeFX> {
             }
         });
         annoContainer.getChildren().add(document.getBodyNode());
+        // Mark as read
+        announceReadHandler.setRead(anno);
+    }
+
+
+    private static class AnnounceReadHandler {
+        public ArkConfig config;
+
+        public AnnounceReadHandler(ArkConfig config) {
+            this.config = config;
+            if (this.config.user_announcement_read == null)
+                this.config.user_announcement_read = new JSONObject();
+        }
+
+        public boolean isRead(AnnounceItem anno) {
+            if (anno == null)
+                return false;
+            String annoId = anno.getAnnoId();
+            if (config.user_announcement_read.containsKey(annoId)) {
+                try {
+                    Number timestamp = config.user_announcement_read.getObject(annoId, Number.class);
+                    return timestamp != null && timestamp.longValue() > 0;
+                } catch (NumberFormatException ignored) {
+                }
+            }
+            return false;
+        }
+
+        public void setRead(AnnounceItem anno) {
+            String annoId = anno.getAnnoId();
+            Logger.debug("Announce", "Read announce id " + annoId);
+            config.user_announcement_read.put(annoId, Instant.now().toEpochMilli() / 1000L);
+            config.save();
+        }
+
+        public void retainAll(List<? extends AnnounceItem> annoList) {
+            Set<String> annoIdSet = annoList.stream().map(AnnounceItem::getAnnoId).collect(Collectors.toSet());
+            int size = config.user_announcement_read.size();
+            if (config.user_announcement_read.keySet().retainAll(annoIdSet)) {
+                int delta = size - config.user_announcement_read.size();
+                Logger.info("Announce", "Removed " + delta + " legacy local announcement records");
+                config.save();
+            }
+        }
     }
 
 
@@ -147,5 +221,23 @@ public final class AnnounceDialog implements Controller<ArkHomeFX> {
         public String markdown;
         /** @since ArkPets 3.7 */ @JSONField
         public String source;
+
+        public String getAnnoId() {
+            return String.format("%08x", hashCode());
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            AnnounceItem that = (AnnounceItem) o;
+            return Objects.equals(title, that.title) && Objects.equals(date, that.date)
+                    && Objects.equals(markdown, that.markdown) && Objects.equals(source, that.source);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(title, date, markdown, source);
+        }
     }
 }
